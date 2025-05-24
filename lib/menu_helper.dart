59aa504f3bff91afa6797f0d388aae2b3d613f40
durand21 <rommendurand77@gmail.com>
2025-05-24 14:12:34 -0600

    creamos menu con prints en pantalla en un archivo aparte

---
+++ lib/menu_helper.dart
@@ -0,0 +1,15 @@
+void mostrarMenuPrincipal() {
+  print('''
+===== CALCULADORA MATEMÁTICA AVANZADA =====
+1. Suma
+2. Resta
+3. Multiplicación
+4. División
+5. Números Primos en un Rango
+6. Números Pares e Impares en un Rango
+7. Secuencia de Fibonacci
+8. Cálculo de Áreas Geométricas
+9. Salir
+==========================================
+''');
+}
